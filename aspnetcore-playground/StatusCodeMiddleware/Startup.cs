0b160ac911aeee8816335680d48a5190ed52a8de
Lars Bergqvist <lars.g.bergqvist@gmail.com>
2021-08-17 10:28:05 +0200

    Added StatusCodePagesMiddleware example

---
+++ aspnetcore-playground/StatusCodeMiddleware/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatusCodeMiddleware
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Routes to the 404 razor page when a 404 error occurs
+            // The placeholder is the status code, e.g. 404
+            app.UseStatusCodePagesWithReExecute("/{0}");
+
+            // Middleware for generating an html response with a description of 400-599 errors
+//            app.UseStatusCodePages();
+
+            // Middleware for serving static files from wwwroot
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
